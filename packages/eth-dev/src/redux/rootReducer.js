280f087c488c893157118959808e06f4c3d79621
MZawnings <96755127+MZawnings@users.noreply.github.com>
2022-01-09 00:45:33 +0200

    Initialize eth-dev

---
+++ packages/eth-dev/src/redux/rootReducer.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux'
+
+import { reducer as levelContainer } from '../components/gameItems/containers/level/controller'
+import { reducer as dialog } from '../components/gameItems/containers/dialog'
+
+import { reducer as background } from '../components/gameItems/components/Background/controller'
+import { reducer as terminal } from '../components/gameItems/components/Terminal/controller'
+import { reducer as wallet } from '../components/gameItems/components/Wallet/controller'
+import { reducer as toolbelt } from '../components/gameItems/components/Toolbelt/controller'
+import { reducer as dish } from '../components/gameItems/components/Dish/controller'
+
+const rootReducer = combineReducers({
+  levelContainer,
+  dialog,
+
+  background,
+  terminal,
+  wallet,
+  toolbelt,
+  dish
+})
+
+export default rootReducer
